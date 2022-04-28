0a6c472477c5e5cb9f19ab1888ec3026202d55b5
fenixkz <fenixkz7@gmail.com>
2022-04-28 17:57:48 +0200

    Updating the gazebo models and the main script

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,8 @@
 #include <yarp/dev/all.h>
 #include <yarp/sig/all.h>
 #include <yarp/math/Math.h>
-#include <yarp/os/Semaphore.h>
+#include <mutex>
+#include <unistd.h>
 
 using namespace std;
 using namespace yarp::os;
@@ -20,26 +21,31 @@ using namespace yarp::math;
 class CtrlModule: public RFModule
 {
 protected:
-    PolyDriver drvArmR, drvArmL, drvHandR, drvHandL;
+    PolyDriver drvArmR, drvArmL, drvHandR, drvHandL, drvTorso;
     ICartesianControl *iarm;
     int startup_ctxt_arm_right;
     int startup_ctxt_arm_left;
     int startup_ctxt_gaze;
     Vector x_init, o_init;
     RpcServer rpcPort;
-    int state;
+    enum class State { upLeft, closeFingers, goRight, goLeft, touch, home, idle, stop, ready};
+    State state;
     int emerStop;
-    Semaphore sem;
-    Semaphore semControl;
+    mutex mutexState;
+    mutex mutexControl;
+    string robot;
     IControlLimits   *ilim;
     IControlMode     *imod;
     IPositionControl *ipos;
-    Vector poseR, poseL;
+    vector<double> poseR, poseL, up_left, homeR, homeL, homeT;
     double trajectory_time, period;
+    void *IControl;
+    double t0 = Time::now();
+    bool cartesian;
     /***************************************************/
     void approachBox()
     {
-        // select the correct interface
+
         drvArmR.view(iarm);
         Vector curDof;
         iarm->getDOF(curDof);
@@ -66,14 +72,7 @@ protected:
         od = yarp::math::dcm2axis(R);
 
         iarm->goToPose(xd, od, trajectory_time);
-        bool done = false;
-        double t0=Time::now();
-        while(!done && (Time::now() - t0 < 10.0) && !emerStop){
-          Time::delay(0.01);
-          semControl.wait();
-          iarm->checkMotionDone(&done);
-          semControl.post();
-        }
+
     }
 
     void moveLeft()
@@ -88,12 +87,9 @@ protected:
         newDof[2] = 0;
         iarm->setDOF(newDof,curDof);
 
-        // enable all dofs but the roll of the torso
         Vector x,o, xd(3), od(4);
         iarm->getPose(x,o);
-        // xd[0]=x[0];
-        // xd[1]=x[1];
-        // xd[2]=x[2];
+
         xd[0] = poseL[0];
         xd[1] = poseL[1];
         xd[2] = poseL[2];
@@ -108,17 +104,8 @@ protected:
         R3=yarp::math::axis2dcm(o3);
         R = R1 * R2 * R3;
         od = yarp::math::dcm2axis(R);
-        // go to the target :)
-        // (in streaming)
+
         iarm->goToPoseSync(xd,od, trajectory_time);
-        bool done = false;
-        double t0=Time::now();
-        while(!done && (Time::now() - t0 < 10.0) && !emerStop){
-          Time::delay(0.01);
-          semControl.wait();
-          iarm->checkMotionDone(&done);
-          semControl.post();
-        }
     }
 
     void moveIndex(){
@@ -136,23 +123,15 @@ protected:
       ienc->getEncoder(j,&enc);
       double target = enc + 2;
       ipos->positionMove(j,target);
-      bool done = false;
-      double t0=Time::now();
-      while(!done && (Time::now() - t0 < 10.0) && !emerStop){
-        Time::delay(0.01);
-        semControl.wait();
-        ipos->checkMotionDone(&done);
-        semControl.post();
-      }
+
     }
 
     void leftHand(){
       drvArmL.view(iarm);
-      Vector x,o, xd(3), od(4);
-      iarm->getPose(x,o);
-      xd[0]=x[0];
-      xd[1]=x[1];
-      xd[2]=x[2] + 0.2;
+      Vector xd(3), od(4);
+      xd[0]=up_left[0];
+      xd[1]=up_left[1];
+      xd[2]=up_left[2];
       Vector o1(4), o2(4);
       Matrix R1, R2, R;
       o1[0] = 0.0; o1[1] = 1.0; o1[2] = 0.0; o1[3] = M_PI;
@@ -161,23 +140,62 @@ protected:
       R2=yarp::math::axis2dcm(o2);
       R = R1 * R2;
       od = yarp::math::dcm2axis(R);
-      // go to the target :)
-      // (in streaming)
-      semControl.wait();
-      iarm->goToPose(xd,od, trajectory_time);
-      semControl.post();
 
-      bool done = false;
-      double t0=Time::now();
-      while((!done || (Time::now() - t0 < 10.0)) && !emerStop){
-        Time::delay(0.01);
-        semControl.wait();
-        iarm->checkMotionDone(&done);
-        semControl.post();
+      iarm->goToPose(xd,od, trajectory_time);
+    }
+
+    bool goHomeTorso(){
+      vector<double> targets;
+      drvTorso.view(imod);
+      drvTorso.view(ipos);
+      targets = homeT;
+      VectorOf<int> Tjoints {0,1,2};
+      VectorOf<int> Tmodes {VOCAB_CM_POSITION, VOCAB_CM_POSITION, VOCAB_CM_POSITION};
+      imod->setControlModes(Tjoints.size(), Tjoints.data(), Tmodes.data());
+      ipos->positionMove(Tjoints.size(), Tjoints.data(), targets.data());
+      return true;
+    }
+    bool goHomeHands(const string hand){
+      vector<double> targets;
+      if (hand == "left"){
+        drvHandL.view(imod);
+        drvHandL.view(ipos);
+        targets = homeL;
+        VectorOf<int> joints {0,1,2,3,4,5,6};
+        VectorOf<int> modes {VOCAB_CM_POSITION, VOCAB_CM_POSITION, VOCAB_CM_POSITION, VOCAB_CM_POSITION,
+        VOCAB_CM_POSITION, VOCAB_CM_POSITION, VOCAB_CM_POSITION};
+        imod->setControlModes(joints.size(), joints.data(), modes.data());
+        ipos->positionMove(joints.size(), joints.data(), targets.data());
+      }else{
+        drvHandR.view(imod);
+        drvHandR.view(ipos);
+        VectorOf<int> joints {0,1,2,3,4,5,6};
+        VectorOf<int> modes {VOCAB_CM_POSITION, VOCAB_CM_POSITION, VOCAB_CM_POSITION, VOCAB_CM_POSITION,
+        VOCAB_CM_POSITION, VOCAB_CM_POSITION, VOCAB_CM_POSITION};
+        targets = homeR;
+        imod->setControlModes(joints.size(), joints.data(), modes.data());
+        ipos->positionMove(joints.size(), joints.data(), targets.data());
       }
+      return true;
     }
 
 
+    State getState(){
+      mutexState.lock();
+      State localState = state;
+      mutexState.unlock();
+      return localState;
+    }
+
+    void setState(const State state_value){
+      if (!emerStop){
+        mutexState.lock();
+        state = state_value;
+        mutexState.unlock();
+      }else{
+        yInfo() << "Emergency stop is active, cannot change the state until the start has pressed";
+      }
+    }
 
     void moveFingers(const string &hand)
     {
@@ -196,56 +214,85 @@ protected:
         }
 
         double min, max, target;
-        VectorOf<double> targets;
-        VectorOf<int> joints {8,9,10,13,14,15};
-
-        for (size_t i=0; i<joints.size(); i++)
+        VectorOf<int> middle {13,14};
+        VectorOf<int> thumb {8,9,10};
+        int pinky = 15;
+        //midle
+        for (size_t i=0; i<middle.size(); i++)
         {
-            int j=joints[i];
-            ilim->getLimits(j,&min,&max);
-            imod->setControlMode(j,VOCAB_CM_POSITION);
-            target = max;
-            targets.push_back(target);
-            // set up the speed in [deg/s]
-            ipos->setRefSpeed(j,30.0);
-
-            // set up max acceleration in [deg/s^2]
-            ipos->setRefAcceleration(j,50.0);
-            ipos->positionMove(j,target);
-            // FILL IN THE CODE
+              int j=middle[i];
+              ilim->getLimits(j,&min,&max);
+              imod->setControlMode(j,VOCAB_CM_POSITION);
+              target = max;
+              // set up the speed in [deg/s]
+              ipos->setRefSpeed(j,30.0);
+              // set up max acceleration in [deg/s^2]
+              ipos->setRefAcceleration(j,50.0);
+              ipos->positionMove(j,target);
         }
-
         bool done = false;
-        double t0=Time::now();
-        while(!done && (Time::now() - t0 < 20.0) && !emerStop){
-          Time::delay(0.01);
-          semControl.wait();
+        while(!done){
+          Time::delay(0.1);
           ipos->checkMotionDone(&done);
-          semControl.post();
         }
+        ilim->getLimits(pinky, &min, &max);
+        imod->setControlMode(pinky,VOCAB_CM_POSITION);
+        target = max;
+        ipos->setRefSpeed(pinky,30.0);
+        ipos->setRefAcceleration(pinky,50.0);
+        ipos->positionMove(pinky,target);
+        done = false;
+        while(!done){
+          Time::delay(0.1);
+          ipos->checkMotionDone(&done);
+        }
+        //thumb
+        for (size_t i=0; i<thumb.size(); i++)
+        {
+              int j=thumb[i];
+              ilim->getLimits(j,&min,&max);
+              imod->setControlMode(j,VOCAB_CM_POSITION);
+              target = max;
+              // set up the speed in [deg/s]
+              ipos->setRefSpeed(j,30.0);
+              // set up max acceleration in [deg/s^2]
+              ipos->setRefAcceleration(j,50.0);
+              ipos->positionMove(j,target);
+        }
+    }
 
+    void moveShoulders(const string hand){
+      int j = 1;
+      PolyDriver &drvHand = (hand=="right"?drvHandR:drvHandL);
+      double target = 90.0;
+      drvHand.view(imod);
+      drvHand.view(ipos);
+      imod->setControlMode(j,VOCAB_CM_POSITION);
+      ipos->positionMove(j,target);
     }
 
     void stop(){
-      semControl.wait();
+      mutexControl.lock();
 
       drvArmL.view(iarm);
       iarm->stopControl();
-      drvArmL.close();
 
       drvArmR.view(iarm);
       iarm->stopControl();
-      drvArmR.close();
 
       drvHandL.view(ipos);
       ipos->stop();
-      drvHandL.close();
-      semControl.post();
+
+      drvHandR.view(ipos);
+      ipos->stop();
+
+      drvTorso.view(ipos);
+      ipos->stop();
+      mutexControl.unlock();
     }
 
 
     bool reset(){
-      string robot="icubSim";
 
       if (!openCartesian(robot,"right_arm"))
           return false;
@@ -326,26 +373,100 @@ protected:
       return true;
     }
 
+    bool openTorso(const string &robot){
+      Property optJoint;
+      optJoint.put("device","remote_controlboard");
+      optJoint.put("remote","/"+robot+"/torso");
+      optJoint.put("local","/position/torso");
+      bool ok=false;
+      double t0=Time::now();
+      while (Time::now()-t0<10.0)
+      {
+          // this might fail if controller
+          // is not connected to solver yet
+          if (drvTorso.open(optJoint))
+          {
+              ok=true;
+              break;
+          }
+
+          Time::delay(1.0);
+      }
+
+      if (!ok)
+      {
+          yError()<<"Unable to open the Torso Controller";
+          return false;
+      }
+      return true;
+    }
+
 public:
     /***************************************************/
     bool configure(ResourceFinder &rf)
     {
-        string robot=rf.check("robot",Value("icubSim")).asString();
+        robot=rf.check("robot",Value("icubSim")).asString();
         // poseR = rf.findGroup("hand right").find("x").asFloat32();
-        poseR.resize(3);
+        Bottle* b = rf.findGroup("right").find("pose").asList();
+        poseR.resize(b->size());
+        for (size_t i = 0; i < b->size(); i++){
+          Value item_v = b->get(i);
+          if (item_v.isNull()){
+            cout << "Error: " << i << " is null" << endl;
+            return false;
+          }
+          poseR.at(i) = item_v.asFloat32();
+        }
 
-        poseR[0] = rf.findGroup("right").check("x", Value(-0.283801)).asFloat32();
-        poseR[1] = rf.findGroup("right").check("y", Value(0.021)).asFloat32();
-        poseR[2] = rf.findGroup("right").check("z", Value(-0.0933)).asFloat32();
+        b = rf.findGroup("left").find("pose").asList();
+        Bottle* bottle_up = rf.findGroup("left").find("up").asList();
+        poseL.resize(b->size());
+        up_left.resize(bottle_up->size());
+        for (size_t i = 0; i < b->size(); i++){
+          Value item_v = b->get(i);
+          Value item_u = bottle_up->get(i);
+          if (item_v.isNull() || item_u.isNull()){
+            cout << "Error: " << i << " is null" << endl;
+            return false;
+          }
+          up_left.at(i) = item_u.asFloat32();
+          poseL.at(i) = item_v.asFloat32();
+        }
 
-        poseL.resize(3);
-        poseL[0] = rf.findGroup("left").check("x", Value(-0.203348)).asFloat32();
-        poseL[1] = rf.findGroup("left").check("y", Value(-0.025)).asFloat32();
-        poseL[2] = rf.findGroup("left").check("z", Value(-0.0933)).asFloat32();
-        trajectory_time = rf.check("trajectory_time", Value(0.0)).asFloat32();
+
+
+        Bottle* bottle_home_left = rf.findGroup("home").find("arm_left").asList();
+        Bottle* bottle_home_right = rf.findGroup("home").find("arm_right").asList();
+
+        homeL.resize(bottle_home_left->size());
+        homeR.resize(bottle_home_right->size());
+        for (size_t i = 0; i < bottle_home_left->size(); i++){
+          Value item_l = bottle_home_left->get(i);
+          Value item_r = bottle_home_right->get(i);
+          if (item_l.isNull() || item_r.isNull()){
+            cout << "Error: " << i << " is null" << endl;
+            return false;
+          }
+          homeL.at(i) = item_l.asFloat32();
+          homeR.at(i) = item_r.asFloat32();
+        }
+
+        b = rf.findGroup("home").find("torso").asList();
+        homeT.resize(b->size());
+        for (size_t i = 0; i < b->size(); i++){
+          Value item_v = b->get(i);
+          if (item_v.isNull()){
+            cout << "Error: " << i << " is null" << endl;
+          }
+          homeT.at(i) = item_v.asFloat32();
+        }
+
+        trajectory_time = rf.check("trajectory_time", Value(10.0)).asFloat32();
         period = rf.check("frequency", Value(1.0)).asFloat32();
         // fprintf(stdout, "%f, %f, %f\n", poseR[0], poseR[1], poseR[2]);
         // fprintf(stdout, "%f, %f, %f\n", poseL[0], poseL[1], poseL[2]);
+        // fprintf(stdout, "%f, %f, %f\n", homeR[0], homeR[1], homeR[2]);
+        // fprintf(stdout, "%f, %f, %f\n", homeL[0], homeL[1], homeL[2]);
         //
         if (!openCartesian(robot,"right_arm"))
             return false;
@@ -353,19 +474,25 @@ public:
         if(!openCartesian(robot, "left_arm"))
             return false;
 
+        if(!openHand(robot, "right_arm"))
+            return false;
+
         if(!openHand(robot, "left_arm"))
+            return false;
+
+        if(!openTorso(robot))
             return false;
         // save startup contexts
         drvArmR.view(iarm);
         iarm->storeContext(&startup_ctxt_arm_right);
         drvArmL.view(iarm);
         iarm->storeContext(&startup_ctxt_arm_left);
-
-        double tmp = Time::now();
-        yInfo() << tmp;
+        // Vector x,o;
+        // iarm->getPose(x,o);
+        // fprintf(stdout, "Left hand %f, %f, %f\n", x[0], x[1], x[2]);
         rpcPort.open("/service");
         attach(rpcPort);
-        state = -1;
+        state = State::ready;
         emerStop = 0;
         return true;
     }
@@ -395,33 +522,27 @@ public:
 
         if (cmd=="goRight")
         {
-            sem.wait();
-            state = 1;
-            sem.post();
+            setState(State::goRight);
             reply.addString("Done");
         }
         else if (cmd == "upLeft"){
-          sem.wait();
-          state = 2;
-          sem.post();
+          setState(State::upLeft);
           reply.addString("Done");
         }
         else if(cmd == "closeFingers"){
-          sem.wait();
-          state = 3;
-          sem.post();
+          setState(State::closeFingers);
           reply.addString("Done");
         }
         else if(cmd == "goLeft"){
-          sem.wait();
-          state = 4;
-          sem.post();
+          setState(State::goLeft);
           reply.addString("Done");
         }
         else if(cmd == "touch"){
-          sem.wait();
-          state = 5;
-          sem.post();
+          setState(State::touch);
+          reply.addString("Done");
+        }
+        else if(cmd == "home"){
+          setState(State::home);
           reply.addString("Done");
         }
         else if(cmd == "stop"){
@@ -429,18 +550,14 @@ public:
           emerStop = 1;
           yInfo() << "Stop has been pressed";
           stop();
-          sem.wait();
-          state = -1;
-          sem.post();
+          setState(State::stop);
           reply.addString("Done");
         }
         else if(cmd == "start"){
-          sem.wait();
-          state = -1;
-          sem.post();
-          yInfo() << "Reset has been pressed";
           emerStop = 0;
-          reset();
+          setState(State::ready);
+          yInfo() << "Reset has been pressed";
+          // reset();
           reply.addString("Done");
         }
         else
@@ -459,43 +576,65 @@ public:
     /***************************************************/
     bool updateModule()
     {
-        switch(state){
-          case 1: // goRight
+        if (!emerStop){
+          State localState = getState();
+          if (localState == State::goRight){
             yInfo() << "Approaching the box";
             approachBox();
-            yInfo() << "Done";
-
-            break;
-          case 2: // upLeft
+            t0=Time::now();
+            cartesian = true;
+            setState(State::idle);
+          }else if (localState == State::upLeft){
             yInfo() << "Moving the left hand";
             leftHand();
-            yInfo() << "Done";
-
-            break;
-          case 3: // closeFingers
+            cartesian = true;
+            t0=Time::now();
+            setState(State::idle);
+          }else if (localState == State::closeFingers){
             yInfo() << "Moving the fingers of the left hand";
             moveFingers("left");
-            yInfo() << "Done";
-
-            break;
-          case 4: // goLeft
+            cartesian = false;
+            t0=Time::now();
+            setState(State::idle);
+          }else if (localState == State::goLeft){
             yInfo() << "Moving the left hand";
             moveLeft();
-            yInfo() << "Done";
-
-            break;
-          case 5: // Touch
+            t0=Time::now();
+            cartesian = true;
+            setState(State::idle);
+          }else if (localState == State::touch){
             yInfo() << "Moving the index finger";
             moveIndex();
-            yInfo() << "Done";
-
-            break;
-          default:
-            break;
+            t0=Time::now();
+            cartesian = false;
+            setState(State::idle);
+          }else if (localState == State::home){
+            yInfo() << "Moving to the home position";
+            moveShoulders("right");
+            usleep(1500000);
+            moveShoulders("left");
+            usleep(1500000);
+            goHomeTorso();
+            usleep(1500000);
+            goHomeHands("left");
+            usleep(1500000);
+            goHomeHands("right");
+            t0=Time::now();
+            cartesian = false;
+            setState(State::idle);
+          }else if (localState == State::idle){
+            bool done = false;
+            cartesian?iarm->checkMotionDone(&done):ipos->checkMotionDone(&done);
+            if (done || ((Time::now() - t0) > 10.0)){
+              setState(State::ready);
+              yInfo() << "Motion has been completed";
+              setState(State::ready);
+            }
+          }
+          else{
+            yInfo() << "Doing nothing";
+          }
         }
-        sem.wait();
-        state = -1;
-        sem.post();
         return true;
     }
 };
